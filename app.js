4a110a1ab14d2549d69c933ba9c812eb327061ab
Prajitha <98685133+prajithas@users.noreply.github.com>
2022-08-01 18:40:53 +0530

    port

--- app.js
+++ app.js
@@ -134,7 +134,7 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/frontend/index.html'));
  });
 
-const port= 5000;
+const port= (process.env.PORT || 5000);
 app.listen(port, (err) => {
   if (err)return console.error(err);
   return console.log(`server is listening on ${port}`);
